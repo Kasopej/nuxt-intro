45d906ae677912287e85c33f691fc45810be6cb9
Kasope Johnson <kasopej@gmail.com>
2023-03-18 15:50:45 +0100

    fix: adds omdb API key to config, styles nav

--- app.vue
+++ app.vue
@@ -1,5 +1,29 @@
 <template>
   <div>
+    <nav style="display: flex">
+      <ul>
+        <li class="nav-item">
+          <nuxt-link to="/">Home</nuxt-link>
+        </li>
+        <li class="nav-item">
+          <nuxt-link to="/movies">Movies</nuxt-link>
+        </li>
+      </ul>
+    </nav>
     <NuxtPage></NuxtPage>
   </div>
 </template>
+
+<script></script>
+
+<style>
+  ul {
+    margin: 0;
+    padding: 0;
+    list-style: none;
+  }
+  .nav-item {
+    display: inline-block;
+    padding: 0 10px;
+  }
+</style>
