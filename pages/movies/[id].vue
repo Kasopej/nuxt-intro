7944151098eaddb3178dd65806915f1895537e20
Kasope Johnson <kasopej@gmail.com>
2023-03-19 00:50:54 +0100

    fix: fetches specific movie with useasynccdata with error handling

--- pages/movies/[id].vue
+++ pages/movies/[id].vue
@@ -1,9 +1,30 @@
 <template>
-  <div>{{ route.params.id }}</div>
+  <div>{{ data }}</div>
 </template>
 
 <script setup>
-  const route = useRoute();
+  const {
+    params: { id },
+  } = useRoute();
+
+  const { data, error } = await useAsyncData(
+    `movies/${id}`,
+    ({ $config }) => {
+      return $fetch(
+        `http://www.omdbapi.com?apiKey=${$config.public.omdbAPI_Key}&i=${id}`
+      );
+    },
+    {
+      transform: ({ Title, Plot, Error }) => {
+        return { Plot, Title, Error };
+      },
+    }
+  );
+
+  if (error.value || data.value.Error) {
+    if (error.value) showError({ statusCode: 500, statusMessage: error });
+    else showError({ statusCode: 404, statusMessage: data.value.Error });
+  }
 </script>
 
 <style lang="scss" scoped></style>
