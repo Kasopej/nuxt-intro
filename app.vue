f8beaa6ab8e36172f7b290240bbcd5699a0acd5a
Kasope Johnson <kasopej@gmail.com>
2023-03-21 10:43:17 +0100

    removes nav styling from app.vue

--- app.vue
+++ app.vue
@@ -9,15 +9,3 @@
     titleTemplate: "%s - Nuxt Movies App",
   });
 </script>
-
-<style>
-  ul {
-    margin: 0;
-    padding: 0;
-    list-style: none;
-  }
-  .nav-item {
-    display: inline-block;
-    padding: 0 10px;
-  }
-</style>
